b6f844ec77933c8721c612f72ae48230d09147ac
KoreyOcampo <koreyocampo@gmail.com>
2024-11-30 12:55:37 -0800

    question options

--- js/main.js
+++ js/main.js
@@ -8,10 +8,20 @@ questions = ( "Which WWE Superstar's finishing move is the Stone Cold Stunner?: 
             "Which WWE Superstar was once known as The Master of the 619:?: ", 
             "Who is the first-ever WWE Superstar to win a title at the WrestleMania event by cashing in their Money in the Bank contract?: ",
             "Which WWE Superstar returned to win the 2023 Women's Royal Rumble match?:",
-            "Who won the first-ever Women's Elimination Chamber match in 2022?"
+            "Who won the first-ever Women's Elimination Chamber match in 2022?")
 
+options = (("A.Bret Hart", "B. Shawn Michaels","C.Stone Cold Steve Austin", "D.The Rock"), 
+           ("A.Ric Flair", "B. Shawn Michaels","C.Mick Foley", "D.Bret Hart")
+           ("A.Dwayne Johnson", "B.Chris Jericho", "C.Randy Orton", "D.John Cena"),
+           ("A.Randy Orton", "B.Kurt Angle","C.Brock Lesnar", "D.Chris Jericho")
+           ("A.Edge", "B.Randy Orton","C.John Cena", "D.Seth Rollins")           
+           ("A.Brock Lesnar", "B. Kane","C.Shawn Michaels", "D.Roman Reigns")
+           ("A.Rey Mysterio", "B.Eddie Guerrero","C.CM Punk", "D.Sin Cara")
+           ("A.Edge", "B.Seth Rollins","C.CM Punk", "D.John Cena")
+           ("A.Becky Lynch", "B. Rhea Ripley","C. Charlotte Flair", "D. Asuka")
+           ("A.Bianca Belair", "B.Rhea Ripley","C.Alexa Bliss", "D.Liv Morgan"))
+           
 
-)
 
   /*----- state variables -----*/
 
