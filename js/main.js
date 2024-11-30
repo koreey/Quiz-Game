cfe39b3f7216a8f8c416adae7cca4c5fe77a3e0d
KoreyOcampo <koreyocampo@gmail.com>
2024-11-30 13:02:19 -0800

    correct  answers

--- js/main.js
+++ js/main.js
@@ -11,17 +11,17 @@ questions = ( "Which WWE Superstar's finishing move is the Stone Cold Stunner?: 
             "Who won the first-ever Women's Elimination Chamber match in 2022?")
 
 options = (("A.Bret Hart", "B. Shawn Michaels","C.Stone Cold Steve Austin", "D.The Rock"), 
-           ("A.Ric Flair", "B. Shawn Michaels","C.Mick Foley", "D.Bret Hart")
+           ("A.Shawn Michaels", "B. Ric Flair","C.Mick Foley", "D.Bret Hart")
            ("A.Dwayne Johnson", "B.Chris Jericho", "C.Randy Orton", "D.John Cena"),
-           ("A.Randy Orton", "B.Kurt Angle","C.Brock Lesnar", "D.Chris Jericho")
+           ("A.Chris Jericho", "B.Kurt Angle","C.Brock Lesnar", "D.Randy Orton")
            ("A.Edge", "B.Randy Orton","C.John Cena", "D.Seth Rollins")           
-           ("A.Brock Lesnar", "B. Kane","C.Shawn Michaels", "D.Roman Reigns")
+           ("A.Shawn Michaels", "B.Kane","C.Brock Lesnar", "D.Roman Reigns")
            ("A.Rey Mysterio", "B.Eddie Guerrero","C.CM Punk", "D.Sin Cara")
            ("A.Edge", "B.Seth Rollins","C.CM Punk", "D.John Cena")
            ("A.Becky Lynch", "B. Rhea Ripley","C. Charlotte Flair", "D. Asuka")
            ("A.Bianca Belair", "B.Rhea Ripley","C.Alexa Bliss", "D.Liv Morgan"))
            
-
+answers = ("C", "B", "A", "D", "A", "C", "A", "B", "B", "A")
 
   /*----- state variables -----*/
 
