5acc8efabbf9de60dbed625b5973004fe4769b35
KoreyOcampo <koreyocampo@gmail.com>
2024-11-30 12:22:06 -0800

    trivia questions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
   /*----- constants -----*/
+questions = ( "Which WWE Superstar's finishing move is the Stone Cold Stunner?: ",
+            "Which WWE Hall of Famer is known for his catchphrase Woo!?: ",  
+            "What is The Rock's real name?:",
+            "Which wrestler is known for his signature move, the RKO?: ",
+            "Which WWE Superstar has the nickname The Rated-R Superstar?: ",
+            "Which wrestler famously defeated The Undertaker at WrestleMania 30 to end his undefeated streak?:",
+            "Which WWE Superstar was once known as The Master of the 619:?: ", 
+            "Who is the first-ever WWE Superstar to win a title at the WrestleMania event by cashing in their Money in the Bank contract?: ",
+            "Which WWE Superstar returned to win the 2023 Women's Royal Rumble match?:",
+            "Who won the first-ever Women's Elimination Chamber match in 2022?"
 
+
+)
 
   /*----- state variables -----*/
 
