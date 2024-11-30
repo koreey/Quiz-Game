69887e28944bb5ab5bb03d6bb1aa232df4f2bacd
KoreyOcampo <koreyocampo@gmail.com>
2024-11-30 15:48:50 -0800

    answers and questions array

--- js/main.js
+++ js/main.js
@@ -1,27 +1,119 @@
   /*----- constants -----*/
-questions = ( "Which WWE Superstar's finishing move is the Stone Cold Stunner?: ",
-            "Which WWE Hall of Famer is known for his catchphrase Woo!?: ",  
-            "What is The Rock's real name?:",
-            "Which wrestler is known for his signature move, the RKO?: ",
-            "Which WWE Superstar has the nickname The Rated-R Superstar?: ",
-            "Which wrestler famously defeated The Undertaker at WrestleMania 30 to end his undefeated streak?:",
-            "Which WWE Superstar was once known as The Master of the 619:?: ", 
-            "Who is the first-ever WWE Superstar to win a title at the WrestleMania event by cashing in their Money in the Bank contract?: ",
-            "Which WWE Superstar returned to win the 2023 Women's Royal Rumble match?:",
-            "Who won the first-ever Women's Elimination Chamber match in 2022?")
+const myQuestions =[
+    {
+        question: "Which WWE Superstar's finishing move is the Stone Cold Stunner?",
+        answers: {
+            A: "Bret Hart",
+            B: "Shawn Michaels",
+            C: "Stone Cold Steve Austin", 
+            D: "The Rock"
+        },
+        correctAnswer: C
+    },
+    {
+        question: "Which WWE Hall of Famer is known for his catchphrase Woo!?" , 
+        answers: {
+            A: "Shawn Michaels",
+            B: "Ric Flair",
+            C: "Mick Foley", 
+            D: "Bret Hart"
 
-options = (("A.Bret Hart", "B. Shawn Michaels","C.Stone Cold Steve Austin", "D.The Rock"), 
-           ("A.Shawn Michaels", "B. Ric Flair","C.Mick Foley", "D.Bret Hart")
-           ("A.Dwayne Johnson", "B.Chris Jericho", "C.Randy Orton", "D.John Cena"),
-           ("A.Chris Jericho", "B.Kurt Angle","C.Brock Lesnar", "D.Randy Orton")
-           ("A.Edge", "B.Randy Orton","C.John Cena", "D.Seth Rollins")           
-           ("A.Shawn Michaels", "B.Kane","C.Brock Lesnar", "D.Roman Reigns")
-           ("A.Rey Mysterio", "B.Eddie Guerrero","C.CM Punk", "D.Sin Cara")
-           ("A.Edge", "B.Seth Rollins","C.CM Punk", "D.John Cena")
-           ("A.Becky Lynch", "B. Rhea Ripley","C. Charlotte Flair", "D. Asuka")
-           ("A.Bianca Belair", "B.Rhea Ripley","C.Alexa Bliss", "D.Liv Morgan"))
-           
-answers = ("C", "B", "A", "D", "A", "C", "A", "B", "B", "A")
+        },
+        correctAnswer: B
+    },
+    {
+         question: "What is The Rock's real name?",
+         answers:{
+            A:"Dwayne Johnson", 
+            B:"Chris Jericho",
+            C:"Randy Orton", 
+            D:"John Cena"
+
+         },
+         correctAnswer: A
+    },
+    {
+          question: "Which wrestler is known for his signature move, the RKO?" ,
+          answers:{
+            A:"Chris Jericho",
+            B: "Kurt Angle",
+            C: "Brock Lesnar", 
+            D:"Randy Orton"
+
+         },
+         correctAnswer: "D"
+    },
+    {
+           question: "Which WWE Superstar has the nickname The Rated-R Superstar?",
+           answers:{
+            A:"Edge", 
+            B:"Randy Orton",
+            C:"John Cena",
+            D:"Seth Rollins"
+
+         },
+         correctAnswer: "A"
+    },
+
+    { 
+            question: "Which wrestler famously defeated The Undertaker at WrestleMania 30 to end his undefeated streak?",
+            answers:{
+               A:"Shawn Michaels", 
+               B:"Kane", 
+               C:"Brock Lesnar", 
+               D:"Roman Reigns"
+
+         },
+         correctAnswer: "C"
+    },
+
+    {
+            question:"Which WWE Superstar was once known as The Master of the 619?", 
+            answers:{
+                A:"Rey Mysterio", 
+                B:"Eddie Guerrero",
+                C:"CM Punk", 
+                D:"Sin Cara"
+
+         },
+         correctAnswer: "A"
+    },
+    {
+            question: "Who is the first-ever WWE Superstar to win a title at the WrestleMania event by cashing in their Money in the Bank contract?" ,
+            answers: {
+                A:"Edge",
+                B:"Seth Rollins",
+                C:"CM Punk",
+                D:"John Cena"
+
+         },
+         correctAnswer: "B"
+    },
+    {
+            question: "Which WWE Superstar returned to win the 2023 Women's Royal Rumble match?",
+            answers:{
+                A:"Becky Lynch",
+                B:"Rhea Ripley",
+                C:"Charlotte Flair",
+                D:"Asuka"
+
+        },
+        correctAnswer: "B"
+    },
+    {
+            questions: "Who won the first-ever Women's Elimination Chamber match in 2022?",
+            answers: {
+                A:"Bianca Belair", 
+                B:"Rhea Ripley",
+                C:"Alexa Bliss", 
+                D:"Liv Morgan"
+
+            },
+            correctAnswer: "A"
+    }
+    ];
+
+const answers = [C, B, A, D, A, C, A, B, B, A]
 
   /*----- state variables -----*/
 
